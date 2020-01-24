87f36a23ce525e7d53693be7ba5143c1bfbb7aca
Pradeep.Kumar <pradeep.kumar@autodatacorp.org>
2020-01-24 16:16:16 +0530

    update latest fiels

--- client/src/Stores/Stores.jsx
+++ client/src/Stores/Stores.jsx
@@ -2,12 +2,13 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 import thunk from "redux-thunk";
+import logger from "redux-logger";
 import Reducers from "./Reducer";
 
 // middleware
-const middleware = [];
+const middleware = [thunk];
 if (process.env.NODE_ENV === "development") {
-  middleware.push(thunk);
+  middleware.push(logger);
 }
 
 const Store = createStore(
